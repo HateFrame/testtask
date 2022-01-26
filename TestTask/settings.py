b5f6665cd3880b0feba4bf88e23c77a299a949db
HateFrame <yurakim2901@gmail.com>
2022-01-27 01:33:34 +0300

    fix settings

--- TestTask/settings.py
+++ TestTask/settings.py
@@ -124,23 +124,11 @@ WSGI_APPLICATION = 'TestTask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_USER_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-        'CONN_MAX_AGE': 500,
-    }
-}
-
+DATABASES = {'default': dj_database_url.config(
+    conn_max_age=600,
+    ssl_require=True,
+)}
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
